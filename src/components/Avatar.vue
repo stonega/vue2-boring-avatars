f19d588f4d4037d936dcb3be3b68853e67fed372
Dave Rupert <rupato@gmail.com>
2021-09-21 03:57:09 -0500

    Remove <main> element from Avatar.vue (#4)
    * Remove <main> element from Avatar.vue
    
    Per MDN, A document mustn't have more than one <main> element that doesn't have the hidden attribute specified.
    
    * Forgot curly brace

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,53 +1,16 @@
 <template>
-  <main>
-    <avatar-pixels
-      v-if="variant === 'pixels'"
-      :name="name"
-      :colors="colors"
-      :size="size"
-    />
-    <avatar-bauhaus
-      v-if="variant === 'bauhaus'"
-      :name="name"
-      :colors="colors"
-      :size="size"
-    />
-    <avatar-marble
-      v-if="variant === 'marble'"
-      :name="name"
-      :colors="colors"
-      :size="size"
-    />
-    <avatar-ring
-      v-if="variant === 'ring'"
-      :name="name"
-      :colors="colors"
-      :size="size"
-    />
-    <avatar-sunset
-      v-if="variant === 'sunset'"
-      :name="name"
-      :colors="colors"
-      :size="size"
-    />
-    <avatar-beam
-      v-if="variant === 'beam'"
-      :name="name"
-      :colors="colors"
-      :size="size"
-    />
-  </main>
+  <component :is="componentName" :name="name" :colors="colors" :size="size" />
 </template>
 
 <script>
-import AvatarPixels from "./AvatarPixels";
-import AvatarBauhaus from "./AvatarBauhaus";
-import AvatarMarble from "./AvatarMarble";
-import AvatarRing from "./AvatarRing";
-import AvatarSunset from "./AvatarSunset";
-import AvatarBeam from "./AvatarBeam";
+import AvatarPixels from './AvatarPixels'
+import AvatarBauhaus from './AvatarBauhaus'
+import AvatarMarble from './AvatarMarble'
+import AvatarRing from './AvatarRing'
+import AvatarSunset from './AvatarSunset'
+import AvatarBeam from './AvatarBeam'
 
-const VARIANTS = ["pixels", "bauhaus", "ring", "beam", "sunset", "marble"];
+const VARIANTS = ['pixels', 'bauhaus', 'ring', 'beam', 'sunset', 'marble']
 
 export default {
   components: {
@@ -56,27 +19,30 @@ export default {
     AvatarMarble,
     AvatarSunset,
     AvatarRing,
-    AvatarBeam,
+    AvatarBeam
   },
   props: {
     variant: {
       type: String,
-      default: "pixels",
-      validator: function (value) {
-        return VARIANTS.indexOf(value) !== -1;
-      },
+      default: 'pixels',
+      validator(value) {
+        return VARIANTS.includes(value)
+      }
     },
-    name: { type: String, default: "Mary Baker" },
+    name: { type: String, default: 'Mary Baker' },
     colors: {
       type: Array,
-      default: () => ["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"],
+      default: () => ['#92A1C6', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90']
     },
-    size: { type: Number, default: 80 },
+    size: { type: Number, default: 80 }
   },
-  data() {
-    return {
-      variants: VARIANTS,
-    };
-  },
-};
+  computed: {
+    componentName() {
+      if (!VARIANTS.includes(this.variant)) {
+        return 'avatar-pixels'
+      }
+      return 'avatar-' + this.variant
+    }
+  }
+}
 </script>
